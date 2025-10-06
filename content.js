1f07aa06f2bb136c0589fb16512f6793e003af69
sahin Islam <sahinislam878@gmail.com>
2025-10-06 13:38:30 +0530

    Detected the Compose Window Successfully!

--- content.js
+++ content.js
@@ -1,1 +1,20 @@
 console.log("Email Writer!");
+const observer = new MutationObserver((mutations) =>{
+     for(const mutation of mutations){
+        const addedNotes = Array.from(mutation.addedNodes); 
+        const hasComposeElements = addedNotes.some(node =>
+            node.nodeType === Node.ELEMENT_NODE &&
+            (node.matches('.aDh, .btC, [role="dialog"]')
+            || node.querySelector('.aDh, .btC, [role="dialog"]'))
+        );
+        if(hasComposeElements){
+            console.log("Compose Window Detected!")
+        }
+    }
+    
+});
+
+observer.observe(document.body,{
+    childList: true,
+    subtree: true
+})
